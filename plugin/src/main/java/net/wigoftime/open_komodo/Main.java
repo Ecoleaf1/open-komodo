710757a13f89851f78811afedff0423078ef0a74
Ecoleaf1 <Ecoleaf@tutamail.com>
2020-06-07 13:44:17 +1000

    Fixed resourcepack crashing

--- plugin/src/main/java/net/wigoftime/open_komodo/Main.java
+++ plugin/src/main/java/net/wigoftime/open_komodo/Main.java
@@ -177,7 +177,7 @@ public class Main extends JavaPlugin implements Listener
 	private static final String phoneName = ChatColor.translateAlternateColorCodes('&', "&e&lEPhone");
 	private static final String cardName = ChatColor.translateAlternateColorCodes('&', "&6&lBank");
 	
-	private static final String resourcePackLink = "https://www.dropbox.com/s/zy8j6alsyk2uk4g/OpenKomodo%20Resourcepack.zip?dl=1";
+	private static final String resourcePackLink = "https://www.dropbox.com/s/9cp1obzlwsd3jsm/OpenKomodo.zip?dl=1";
 	
 	private static JavaPlugin plugin;
 	
