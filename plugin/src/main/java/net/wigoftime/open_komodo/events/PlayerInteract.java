aa927582dc617de3d6acc98e3041864686e6c9fd
Ecoleaf1 <Ecoleaf@tutamail.com>
2021-03-31 19:27:03 +1100

    Neated code up a bit, fixed hat prop being replaced

--- plugin/src/main/java/net/wigoftime/open_komodo/events/PlayerInteract.java
+++ plugin/src/main/java/net/wigoftime/open_komodo/events/PlayerInteract.java
@@ -24,168 +24,134 @@ import net.wigoftime.open_komodo.objects.CustomPlayer;
 import net.wigoftime.open_komodo.objects.ItemType;
 
 public class PlayerInteract implements EventExecutor {
-
+	
+	private static boolean isItemEquipable(ItemStack itemStack) {
+		if (itemStack.getType() != Material.INK_SAC) return false;
+		
+		ItemMeta meta = itemStack.getItemMeta();
+		
+		if (!(meta.hasCustomModelData())) return false;
+		
+		CustomItem itemCustom = CustomItem.getCustomItem(meta.getCustomModelData());
+		if (itemCustom == null) return false;
+				
+		return itemCustom.isEquipable() ? true : false;
+	}
+	
+	private static boolean specialItem(ItemStack itemStack, CustomPlayer playerCustom, PlayerInteractEvent event) {
+		if (!(itemStack.getItemMeta().hasCustomModelData())) return false;
+		
+		switch(itemStack.getType()) {
+		case INK_SAC:
+			if (playerCustom.hasActiveGui()) return false;
+			
+			CustomItem itemCustom = CustomItem.getCustomItem(itemStack.getItemMeta().getCustomModelData());
+			if (itemCustom == null) return false;
+			
+			if (itemCustom.getType() == ItemType.PHONE) {
+				CustomGUI gui = new PhoneGui(playerCustom);
+				gui.open();
+			} else if (itemCustom.getItem().getItemMeta().getCustomModelData() == 56) {
+				CurrencyClass.displayBalance(playerCustom);
+			} else if (isItemEquipable(itemStack)) {
+				ItemStack currentHat = playerCustom.getPlayer().getInventory().getHelmet();
+				
+				
+				if (currentHat != null) {
+					CustomItem currentHatCustom = CustomItem.getCustomItem(currentHat.getItemMeta().getCustomModelData());
+					if (currentHatCustom != null)
+					if (currentHatCustom.getType() == ItemType.PROP) playerCustom.getPlayer().getInventory().addItem(new ItemStack(currentHat));
+					
+					currentHat.setAmount(0);
+				}
+				playerCustom.getPlayer().getInventory().setHelmet(new ItemStack(itemStack));
+				itemStack.setAmount(0);
+				
+			}
+			
+			event.setCancelled(true);
+			return true;
+		case STICK:
+			InventoryManagement.openBagInventory(playerCustom, itemStack.getItemMeta().getCustomModelData());
+			event.setCancelled(true);
+			return true;
+		default:
+			return false;
+		}
+	}
+	
 	@Override
 	public void execute(Listener listener, Event event) throws EventException {
 		PlayerInteractEvent playerInteractEvent = (PlayerInteractEvent) event;
 		
-		if (playerInteractEvent.getAction() == Action.PHYSICAL)
-		{
-			if (playerInteractEvent.getClickedBlock().getType() == Material.FARMLAND)
-				playerInteractEvent.setCancelled(true);
-			
-			return;
-		}
-		
-		// Get player in CustomPlayer format
 		CustomPlayer player = CustomPlayer.get(playerInteractEvent.getPlayer().getUniqueId());
 		
 		// If customplayer format of it doesn't exist, cancel
 		// Can happen if the server hasn't loaded all of the player information when they join
-		if (player == null)
-		{
+		if (player == null) {
 			playerInteractEvent.setCancelled(true);
 			return;
 		}
 		
-		// Get Action
-		Action action = playerInteractEvent.getAction();
-		// Get Item
-		ItemStack item = playerInteractEvent.getItem();
-			
-		// If not left and right click on block, cancel it.
-		// This is to prevent players quickly moving their cursor out right
-		// when they left click on the fire to put it out.
-		if (action != Action.LEFT_CLICK_BLOCK && action != Action.RIGHT_CLICK_BLOCK)
-			playerInteractEvent.setCancelled(true);
-		
-			// If clicked on fire
-			if (player.getPlayer().getTargetBlock((Set<Material>) null, 20).getType() == Material.FIRE)
-			{
-				// If player does not have permission, cancel and return
-				if (!player.getPlayer().hasPermission(Permissions.changePerm))
-				{
-					player.getPlayer().sendMessage(Permissions.getChangeError());
-					
-					playerInteractEvent.setCancelled(true);
-				}
-				
-				return;
-			}
-		
-		// If right clicked on block
-		if (action == Action.RIGHT_CLICK_BLOCK)
-		{
-			// Get block
-			Block block = playerInteractEvent.getClickedBlock();
-			// Get Material
-			Material material = block.getType();
-			
-			if (material == Material.OAK_TRAPDOOR || material == Material.SPRUCE_TRAPDOOR || material == Material.JUNGLE_TRAPDOOR || 
-					material == Material.DARK_OAK_TRAPDOOR || material == Material.BIRCH_TRAPDOOR || material == Material.ACACIA_TRAPDOOR || 
-					material == Material.DISPENSER || material == Material.DROPPER) {	
-				playerInteractEvent.setCancelled(true);
-				return;
-			}
-			
-			if (material == Material.FLOWER_POT || material.name().startsWith("POTTED"))
-			{
+		if (player.getPlayer().getTargetBlock((Set<Material>) null, 20).getType() == Material.FIRE) {
+			if (!player.getPlayer().hasPermission(Permissions.changePerm)){
 				player.getPlayer().sendMessage(Permissions.getChangeError());
 				playerInteractEvent.setCancelled(true);
-				return;
 			}
 			
-			if (material.name().endsWith("SHULKER_BOX"))
-			{
-				playerInteractEvent.setCancelled(true);
-				return;
-			}
-			
-			// If Enderchest
-			if (block.getType() == Material.ENDER_CHEST)
-			{
-				// Allow open if player has permission
-				if (player.getPlayer().hasPermission(Permissions.changePerm))
-					return;
-				
-				if (player.isBuilding())
-					return;
-				
-				// Other wise, cancel it
-				playerInteractEvent.setCancelled(true);
-				player.getPlayer().sendMessage(Permissions.useError);
-				return;
-			}
-		}
-		
-		if (item == null)
 			return;
-		
-		if (action == Action.RIGHT_CLICK_BLOCK)
-		if (player.getPlayer().hasPermission(Permissions.changePerm))
-			if (player.isBuilding()) {
-				if (FurnitureMangement.isValid(item)) {
-					playerInteractEvent.setCancelled(true);
-					FurnitureMangement.createFurniture(item, playerInteractEvent);
-				}
-				
-				return;
-			}
-		
-		if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK)
-		{
-			if (item.getType() == Material.INK_SAC)
-			{
-				ItemMeta meta = item.getItemMeta();
-				if (meta.hasCustomModelData())
-				{
-					CustomItem cs = CustomItem.getCustomItem(meta.getCustomModelData());
-					
-					if (cs == null)
-						return;
-					
-					if (cs.isEquipable())
-					{
-						player.getPlayer().getInventory().setHelmet(item);
-						item.setAmount(0);
-					}
-				}
-			}
 		}
 		
-		// Checks if Item could have an ID
-		if (item.getItemMeta().hasCustomModelData())
-		{
-			// If ID is 1 (IPlay Phone)
-			if (item.getType() == Material.INK_SAC && !player.hasActiveGui())
-			{
-				// And if the item is an ink sac, open IPlay
-				CustomItem customItem = CustomItem.getCustomItem(item.getItemMeta().getCustomModelData());
+		ItemStack item = playerInteractEvent.getItem();
+		actionManage(playerInteractEvent, player, item);
+		if (item == null) return;
+		
+		
+		// If special item, call function and return
+		if (specialItem(item, player, playerInteractEvent)) return;
+	}
+	
+	private void actionManage(PlayerInteractEvent event, CustomPlayer playerCustom, ItemStack itemStack) {
+		switch(event.getAction()) {
+		case PHYSICAL:
+			if (event.getClickedBlock().getType() == Material.FARMLAND) event.setCancelled(true);
+			break;
+		case RIGHT_CLICK_BLOCK:
+			Block clickedBlock = event.getClickedBlock();
+			
+			if (clickedBlock.getType() == Material.OAK_TRAPDOOR || clickedBlock.getType() == Material.SPRUCE_TRAPDOOR || clickedBlock.getType() == Material.JUNGLE_TRAPDOOR || 
+					clickedBlock.getType() == Material.DARK_OAK_TRAPDOOR || clickedBlock.getType() == Material.BIRCH_TRAPDOOR || clickedBlock.getType() == Material.ACACIA_TRAPDOOR || 
+					clickedBlock.getType() == Material.DISPENSER || clickedBlock.getType() == Material.DROPPER) {	
+				event.setCancelled(true);
+				break;
+			} else if (clickedBlock.getType() == Material.FLOWER_POT || clickedBlock.getType().name().startsWith("POTTED")) {
+				playerCustom.getPlayer().sendMessage(Permissions.getChangeError());
+				event.setCancelled(true);
+				break;
+			} else if (clickedBlock.getType().name().endsWith("SHULKER_BOX")) {
+				event.setCancelled(true);
+				break;
+			} else if (clickedBlock.getType() == Material.ENDER_CHEST) {
+				if (playerCustom.getPlayer().hasPermission(Permissions.changePerm)) break;
+				if (playerCustom.isBuilding()) break;
 				
-				if (customItem == null)
-					return;
-				
-				if (customItem.getType() == ItemType.PHONE) {	
-					playerInteractEvent.setCancelled(true);
-					CustomGUI gui = new PhoneGui(player);
-					gui.open();
-					//PhoneGui.open(player);
-					return;
-				}
-				
-				if (item.getItemMeta().getCustomModelData() == 56)
-				{
-					playerInteractEvent.setCancelled(true);
-					CurrencyClass.displayBalance(player);
-					return;
-				}
+				event.setCancelled(true);
+				playerCustom.getPlayer().sendMessage(Permissions.useError);
+				break;
 			}
 			
-			if (item.getType() == Material.STICK)
-			{
-				InventoryManagement.openBagInventory(player, item.getItemMeta().getCustomModelData());
-			}
+			if (itemStack == null) break;
+			
+			if (playerCustom.getPlayer().hasPermission(Permissions.changePerm))
+				if (playerCustom.isBuilding()) {
+					if (FurnitureMangement.isValid(itemStack)) {
+						event.setCancelled(true);
+						FurnitureMangement.createFurniture(itemStack, event);
+					}
+					
+					break;
+				}
+			break;
 		}
 	}
-
 }
